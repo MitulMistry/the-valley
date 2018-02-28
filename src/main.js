9c40fc2cdf7be007b3002ec3cc5330ebb9c8128b
MitulMistry <MitulMistryDev@gmail.com>
2018-02-27 22:49:29 -0600

    Update index and main.js with code from phaser-es6-webpack and import states

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 // import 'jquery';
+import 'pixi';
+import 'p2';
 import Phaser from 'phaser';
+
+import boot from './states/stateBoot';
+import preload from './states/statePreload';
+import splash from './states/stateSplash';
+import mainMenu from './states/stateMenu';
+import preload2 from './states/statePreload2';
+import theGame from './states/stateAdventure';
 
 (function() {
   var game = new Phaser.Game(800, 600, Phaser.CANVAS, "game");
