43d21e34b022a316ce60580e39ee78731faa9e00
MitulMistry <MitulMistryDev@gmail.com>
2018-03-06 18:09:00 -0600

    Clean up code for Boot and Splash states

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -12,7 +12,8 @@ export default class extends Phaser.State {
 		// this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		// this.scale.pageAlignHorizontally = true;
 		// NOT USED: this.scale.pageAlignVertically = true;
-		// this.scale.setScreenSize(true);
+		// DEPRECATED: this.scale.setScreenSize(true);
+		// this.game.scale.refresh();
 
 		this.fontsReady = false;
 		this.fontsLoaded = this.fontsLoaded.bind(this);
@@ -35,10 +36,10 @@ export default class extends Phaser.State {
 
 	render() {
 		if (config.webfonts.length && this.fontsReady) {
-      this.state.start('MenuPreload')
+      this.state.start('MenuPreload');
     }
     if (!config.webfonts.length) {
-      this.state.start('MenuPreload')
+      this.state.start('MenuPreload');
     }
 	}
 
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -3,6 +3,10 @@ import Phaser from 'phaser';
 import globals from '../globals/globals';
 
 export default class extends Phaser.State {
+	init() {
+		this.stage.backgroundColor = '#000000';
+	}
+
 	create() {
 		// music = this.add.audio("01Ambient");
 		// music.volume = .5;
@@ -16,8 +20,6 @@ export default class extends Phaser.State {
 		var delay = 1500;
 		var fadeOutLength = 300;
 
-		this.stage.backgroundColor = '#000000';
-
 		if (!globals.debugMode) {
 			var logo = this.add.sprite(this.game.width / 2, this.game.height / 2, "splashLogo");
 			logo.anchor.setTo(0.5, 0.5);
@@ -28,26 +30,20 @@ export default class extends Phaser.State {
 			blackFade.width = this.game.width;
 			this.add.tween(blackFade).to({ alpha: 0 }, fadeInLength, Phaser.Easing.Linear.None, true);
 
+			//Fade out
 			this.time.events.add(fadeInLength + delay, function () {
-			//this.time.events.add(1500, function () {
-
-				//Fade out
 				var blackFade2 = this.add.sprite(0, 0, "rectangle_black");
 				blackFade2.height = this.game.height;
 				blackFade2.width = this.game.width;
 				blackFade2.alpha = 0;
 				this.add.tween(blackFade2).to({ alpha: 1 }, fadeOutLength, Phaser.Easing.Linear.None, true);
-
 			}, this);
 
 			this.time.events.add(fadeInLength + delay + fadeOutLength, function () {
-
 				this.state.start("Menu");
-
 			}, this);
-		}
-		else
-		{
+
+		} else {
 			this.state.start("Menu");
 		}
 
