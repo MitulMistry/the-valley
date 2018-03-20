5df60a09d85468c4d4797d66377f4447a210d907
MitulMistry <MitulMistryDev@gmail.com>
2018-03-20 13:43:41 -0500

    Shorten variable names in SaveGame methods

--- src/mechanics/SaveGame.js
+++ src/mechanics/SaveGame.js
@@ -49,110 +49,110 @@ import constants from '../globals/constants';
 	}
 
 	// Write story node outcome to gameLog
-	writeToGameLog (referenceNode01, decision01) {
-		this.gameLog.push( { referenceNode: referenceNode01, decision: decision01 } );
+	writeToGameLog (textNodeKey, decision) {
+		this.gameLog.push( { textNodeKey: textNodeKey, decision: decision } );
 	}
 
 	// Write additional variables to data structure
-	writeToGameVariables (reference01, value01) {
+	writeToGameVariables (reference, value) {
 
 		// Check if the variable already exists. If it does, update it.
-		if (this.gameVariables.has(reference01)) {
-			let original = this.gameVariables.get(reference01);
-			this.gameVariables.set(reference01, original + value01);
+		if (this.gameVariables.has(reference)) {
+			let original = this.gameVariables.get(reference);
+			this.gameVariables.set(reference, original + value);
 		} else {
-			this.gameVariables.set(reference01, value01);
+			this.gameVariables.set(reference, value);
 		}
 	}
 
 	// Check for additional variables in data structure
-	checkGameVariables (reference01, equivalence01, value01) {
+	checkGameVariables (reference, equivalence, value) {
 		// search for reference and value pair in gameVariables data structure
 		// if found, checks for whether it's >, <, etc. to the value provided
 		// if it doesn't pass the test to the value, or if not found, it returns false
 
 		var defaultValue = 0;
 
-		if (equivalence01 === '' || equivalence01 === null || equivalence01 === undefined)
+		if (equivalence === '' || equivalence === null || equivalence === undefined)
 		{
 			// just search for whether the additional variable is present - value doesn't matter
-			if (this.gameVariables.has(reference01)) {
+			if (this.gameVariables.has(reference)) {
 				return true;
 			} else {
 				return false;
 			}
 		}
-		else if (equivalence01 === '=')
+		else if (equivalence === '=')
 		{
 			// check for presence of variable and value
-			if (this.gameVariables.get(reference01) === value01) {
+			if (this.gameVariables.get(reference) === value) {
 				return true;
 			} else {
 				return false;
 			}
 		}
-		else if (equivalence01 === '!=' && (value01 === '' || value01 === null || value01 === undefined)) {
+		else if (equivalence === '!=' && (value === '' || value === null || value === undefined)) {
 			// checks for if the additional variable is present at all, and returns false if present, true if not - opposite of first check in this series. e.g. if !(01JennethDead), then returns true.
-			if (this.gameVariables.has(reference01)) {
+			if (this.gameVariables.has(reference)) {
 				return false;
 			} else {
 				return true;
 			}
 		}
-		else if (equivalence01 === '!=' && !(value01 === '' || value01 === null || value01 === undefined)) {
-			if (this.gameVariables.has(reference01) && this.gameVariables.get(reference01) !== value01) {
+		else if (equivalence === '!=' && !(value === '' || value === null || value === undefined)) {
+			if (this.gameVariables.has(reference) && this.gameVariables.get(reference) !== value) {
 				return true;
 			}
 
-			if (value01 != defaultValue) {
+			if (value != defaultValue) {
 				//variable not found, so assume default negative
 				return true;
 			}
 			return false;
 		}
-		else if (equivalence01 === '<')
+		else if (equivalence === '<')
 		{
-			if (this.gameVariables.has(reference01) && this.gameVariables.get(reference01) < value01) {
+			if (this.gameVariables.has(reference) && this.gameVariables.get(reference) < value) {
 				return true;
 			}
 
-			if (value01 < defaultValue) {
+			if (value < defaultValue) {
 				//variable not found, so assume default negative
 				return true;
 			}
 			return false;
 		}
-		else if (equivalence01 === '<=')
+		else if (equivalence === '<=')
 		{
-			if (this.gameVariables.has(reference01) && this.gameVariables.get(reference01) <= value01) {
+			if (this.gameVariables.has(reference) && this.gameVariables.get(reference) <= value) {
 				return true;
 			}
 
-			if (value01 <= defaultValue) {
+			if (value <= defaultValue) {
 				//variable not found, so assume default negative
 				return true;
 			}
 			return false;
 		}
-		else if (equivalence01 === '>')
+		else if (equivalence === '>')
 		{
-			if (this.gameVariables.has(reference01) && this.gameVariables.get(reference01) > value01) {
+			if (this.gameVariables.has(reference) && this.gameVariables.get(reference) > value) {
 				return true;
 			}
 
-			if (value01 > defaultValue) {
+			if (value > defaultValue) {
 				//variable not found, so assume default negative
 				return true;
 			}
 			return false;
 		}
-		else if (equivalence01 === '>=')
+		else if (equivalence === '>=')
 		{
-			if (this.gameVariables.has(reference01) && this.gameVariables.get(reference01) >= value01) {
+			if (this.gameVariables.has(reference) && this.gameVariables.get(reference) >= value) {
 				return true;
 			}
 
-			if (value01 >= defaultValue) {
+			if (value >= defaultValue) {
 				//variable not found, so assume default negative
 				return true;
 			}
