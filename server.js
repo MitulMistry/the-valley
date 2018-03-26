bc52ea37661343690786072f4c9668c8e761c9b0
MitulMistry <MitulMistryDev@gmail.com>
2018-03-26 15:34:20 -0500

    Set up project for Heroku deployment

---
+++ server.js
@@ -0,0 +1,11 @@
+// https://github.com/heroku/node-js-getting-started/blob/master/index.js
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+    .use(express.static(__dirname))
+    .set('views', __dirname)
+    .engine('html', require('ejs').renderFile)
+    .get('*', (req, res) => res.render('index.html'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
