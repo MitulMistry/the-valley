827180b07331433ae229d7504d16c9716be2b852
MitulMistry <MitulMistryDev@gmail.com>
2018-03-26 22:25:40 -0500

    Add view engine

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 express()
     .use(express.static(__dirname))
     .set('views', path.join(__dirname, 'build'))
+    .set('view engine', 'ejs')
     .engine('html', require('ejs').renderFile)
     .get('/', (req, res) => res.render('index.html'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
