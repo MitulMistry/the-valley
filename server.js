99dc33b3b579b8e9d430cac4e7875e0dfa78c4e3
MitulMistry <MitulMistryDev@gmail.com>
2018-03-27 01:13:57 -0500

    Switch server.js to older Express syntax to fix errors on Heroku deployment

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-// https://github.com/heroku/node-js-getting-started/blob/master/index.js
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+var express = require('express'); // https://github.com/heroku/node-js-getting-started/blob/master/index.js
 
-express()
-    .use(express.static(__dirname))
-    .set('views', path.join(__dirname, 'build'))
-    .engine('html', require('ejs').renderFile)
-    .get('/', (req, res) => res.render('index.html'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var app = express();
+
+app.set('port', (process.env.PORT || 5000)); // gets port from environment or else defaults to 5000
+
+app.use(express.static(__dirname + '/build')); // read files from this folder
+
+app.get('/', function(req, res) { // request, response
+  res.render('index.html');
+});
+
+// app.get('*', function (req, res) { // This wildcard method (catch-all) handles all other requests - keep on bottom
+//   res.redirect('/');
+// });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
