fb98f45fa7f33320c768c926795bc0c7b84d66c7
MitulMistry <MitulMistryDev@gmail.com>
2018-03-26 22:38:28 -0500

    Switch to old Express syntax

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 // https://github.com/heroku/node-js-getting-started/blob/master/index.js
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+// const express = require('express');
+// const path = require('path');
+// const PORT = process.env.PORT || 5000;
 
-express()
-    .use(express.static(__dirname))
-    .set('views', path.join(__dirname, 'build'))
-    .set('view engine', 'ejs')
-    .engine('html', require('ejs').renderFile)
-    .get('/', (req, res) => res.render('index.html'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// express()
+//     .use(express.static(__dirname))
+//     .set('views', path.join(__dirname, 'build'))
+//     .set('view engine', 'ejs')
+//     .engine('html', require('ejs').renderFile)
+//     .get('/', (req, res) => res.render('index.html'))
+//     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+var express = require('express'); //https://github.com/heroku/node-js-getting-started/blob/master/index.js
+// var cors = require('cors');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000)); //gets port from environment or else defaults to 5000
+
+// app.use(cors()); //enable all CORS requests
+app.use(express.static(__dirname + '/build')); //read files from this folder
+
+app.get('/', function(req, res) { //request, response
+  res.render('index.html');
+});
+
+// app.get('*', function (req, res) { // This wildcard method (catch-all) handles all other requests - keep on bottom
+//   res.redirect('/');
+// });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
