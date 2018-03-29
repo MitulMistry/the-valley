ea827e1501d59f48a1b58f457bbd7bb9eb9763ab
MitulMistry <MitulMistryDev@gmail.com>
2018-03-29 13:20:49 -0500

    Merge master

--- server.js
+++ server.js
@@ -1,27 +1,12 @@
-// https://github.com/heroku/node-js-getting-started/blob/master/index.js
-// const express = require('express');
-// const path = require('path');
-// const PORT = process.env.PORT || 5000;
-
-// express()
-//     .use(express.static(__dirname))
-//     .set('views', path.join(__dirname, 'build'))
-//     .set('view engine', 'ejs')
-//     .engine('html', require('ejs').renderFile)
-//     .get('/', (req, res) => res.render('index.html'))
-//     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-var express = require('express'); //https://github.com/heroku/node-js-getting-started/blob/master/index.js
-// var cors = require('cors');
+var express = require('express'); // https://github.com/heroku/node-js-getting-started/blob/master/index.js
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000)); //gets port from environment or else defaults to 5000
+app.set('port', (process.env.PORT || 5000)); // gets port from environment or else defaults to 5000
 
-// app.use(cors()); //enable all CORS requests
-app.use(express.static(__dirname + '/build')); //read files from this folder
+app.use(express.static(__dirname + '/build')); // read files from this folder
 
-app.get('/', function(req, res) { //request, response
+app.get('/', function(req, res) { // request, response
   res.render('index.html');
 });
 
